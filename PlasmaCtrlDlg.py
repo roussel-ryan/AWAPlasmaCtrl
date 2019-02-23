a042729d177b403f12c7dfe9eea51cd7099b9b88
U-awa3\awa <awa@awa3.hep.anl.gov>
2019-02-22 18:37:57 -0600

    added solenoid controller

--- PlasmaCtrlDlg.py
+++ PlasmaCtrlDlg.py
@@ -47,85 +47,85 @@ class Ui_PlasmaCtrlDlg(object):
         self.label_15 = QtWidgets.QLabel(self.IonGaugeCtrl)
         self.label_15.setGeometry(QtCore.QRect(10, 110, 31, 19))
         self.label_15.setObjectName("label_15")
-        self.DCR55_180T = QtWidgets.QGroupBox(PlasmaCtrlDlg)
-        self.DCR55_180T.setGeometry(QtCore.QRect(20, 10, 251, 131))
-        self.DCR55_180T.setObjectName("DCR55_180T")
-        self.label_3 = QtWidgets.QLabel(self.DCR55_180T)
+        self.SOLENOID_180T = QtWidgets.QGroupBox(PlasmaCtrlDlg)
+        self.SOLENOID_180T.setGeometry(QtCore.QRect(20, 10, 251, 131))
+        self.SOLENOID_180T.setObjectName("SOLENOID_180T")
+        self.label_3 = QtWidgets.QLabel(self.SOLENOID_180T)
         self.label_3.setGeometry(QtCore.QRect(20, 60, 21, 19))
         self.label_3.setObjectName("label_3")
-        self.label_4 = QtWidgets.QLabel(self.DCR55_180T)
+        self.label_4 = QtWidgets.QLabel(self.SOLENOID_180T)
         self.label_4.setGeometry(QtCore.QRect(50, 30, 64, 19))
         self.label_4.setObjectName("label_4")
-        self.label_5 = QtWidgets.QLabel(self.DCR55_180T)
+        self.label_5 = QtWidgets.QLabel(self.SOLENOID_180T)
         self.label_5.setGeometry(QtCore.QRect(150, 30, 64, 19))
         self.label_5.setObjectName("label_5")
-        self.label_6 = QtWidgets.QLabel(self.DCR55_180T)
+        self.label_6 = QtWidgets.QLabel(self.SOLENOID_180T)
         self.label_6.setGeometry(QtCore.QRect(23, 90, 16, 20))
         self.label_6.setObjectName("label_6")
-        self.DCR55VSet = QtWidgets.QLineEdit(self.DCR55_180T)
-        self.DCR55VSet.setGeometry(QtCore.QRect(40, 50, 81, 31))
-        self.DCR55VSet.setObjectName("DCR55VSet")
-        self.DCR55ISet = QtWidgets.QLineEdit(self.DCR55_180T)
-        self.DCR55ISet.setGeometry(QtCore.QRect(40, 80, 81, 31))
-        self.DCR55ISet.setObjectName("DCR55ISet")
-        self.DCR55VRead = QtWidgets.QLineEdit(self.DCR55_180T)
-        self.DCR55VRead.setGeometry(QtCore.QRect(130, 50, 91, 31))
-        self.DCR55VRead.setObjectName("DCR55VRead")
-        self.DCR55IRead = QtWidgets.QLineEdit(self.DCR55_180T)
-        self.DCR55IRead.setGeometry(QtCore.QRect(130, 80, 91, 31))
-        self.DCR55IRead.setObjectName("DCR55IRead")
-        self.DCR55_180T_2 = QtWidgets.QGroupBox(PlasmaCtrlDlg)
-        self.DCR55_180T_2.setGeometry(QtCore.QRect(20, 150, 251, 131))
-        self.DCR55_180T_2.setObjectName("DCR55_180T_2")
-        self.label_7 = QtWidgets.QLabel(self.DCR55_180T_2)
+        self.SOLENOIDVSet = QtWidgets.QLineEdit(self.SOLENOID_180T)
+        self.SOLENOIDVSet.setGeometry(QtCore.QRect(40, 50, 81, 31))
+        self.SOLENOIDVSet.setObjectName("SOLENOIDVSet")
+        self.SOLENOIDISet = QtWidgets.QLineEdit(self.SOLENOID_180T)
+        self.SOLENOIDISet.setGeometry(QtCore.QRect(40, 80, 81, 31))
+        self.SOLENOIDISet.setObjectName("SOLENOIDISet")
+        self.SOLENOIDVRead = QtWidgets.QLineEdit(self.SOLENOID_180T)
+        self.SOLENOIDVRead.setGeometry(QtCore.QRect(130, 50, 91, 31))
+        self.SOLENOIDVRead.setObjectName("SOLENOIDVRead")
+        self.SOLENOIDIRead = QtWidgets.QLineEdit(self.SOLENOID_180T)
+        self.SOLENOIDIRead.setGeometry(QtCore.QRect(130, 80, 91, 31))
+        self.SOLENOIDIRead.setObjectName("SOLENOIDIRead")
+        self.SOLENOID_180T_2 = QtWidgets.QGroupBox(PlasmaCtrlDlg)
+        self.SOLENOID_180T_2.setGeometry(QtCore.QRect(20, 150, 251, 131))
+        self.SOLENOID_180T_2.setObjectName("SOLENOID_180T_2")
+        self.label_7 = QtWidgets.QLabel(self.SOLENOID_180T_2)
         self.label_7.setGeometry(QtCore.QRect(20, 60, 21, 19))
         self.label_7.setObjectName("label_7")
-        self.label_8 = QtWidgets.QLabel(self.DCR55_180T_2)
+        self.label_8 = QtWidgets.QLabel(self.SOLENOID_180T_2)
         self.label_8.setGeometry(QtCore.QRect(50, 30, 64, 19))
         self.label_8.setObjectName("label_8")
-        self.label_9 = QtWidgets.QLabel(self.DCR55_180T_2)
+        self.label_9 = QtWidgets.QLabel(self.SOLENOID_180T_2)
         self.label_9.setGeometry(QtCore.QRect(150, 30, 64, 19))
         self.label_9.setObjectName("label_9")
-        self.label_10 = QtWidgets.QLabel(self.DCR55_180T_2)
+        self.label_10 = QtWidgets.QLabel(self.SOLENOID_180T_2)
         self.label_10.setGeometry(QtCore.QRect(23, 90, 16, 20))
         self.label_10.setObjectName("label_10")
-        self.HEATERVSet = QtWidgets.QLineEdit(self.DCR55_180T_2)
+        self.HEATERVSet = QtWidgets.QLineEdit(self.SOLENOID_180T_2)
         self.HEATERVSet.setGeometry(QtCore.QRect(40, 50, 81, 31))
         self.HEATERVSet.setObjectName("HEATERVSet")
-        self.HEATERISet = QtWidgets.QLineEdit(self.DCR55_180T_2)
+        self.HEATERISet = QtWidgets.QLineEdit(self.SOLENOID_180T_2)
         self.HEATERISet.setGeometry(QtCore.QRect(40, 80, 81, 31))
         self.HEATERISet.setObjectName("HEATERISet")
-        self.HEATERVRead = QtWidgets.QLineEdit(self.DCR55_180T_2)
+        self.HEATERVRead = QtWidgets.QLineEdit(self.SOLENOID_180T_2)
         self.HEATERVRead.setGeometry(QtCore.QRect(130, 50, 91, 31))
         self.HEATERVRead.setObjectName("HEATERVRead")
-        self.HEATERIRead = QtWidgets.QLineEdit(self.DCR55_180T_2)
+        self.HEATERIRead = QtWidgets.QLineEdit(self.SOLENOID_180T_2)
         self.HEATERIRead.setGeometry(QtCore.QRect(130, 80, 91, 31))
         self.HEATERIRead.setObjectName("HEATERIRead")
-        self.DCR55_180T_3 = QtWidgets.QGroupBox(PlasmaCtrlDlg)
-        self.DCR55_180T_3.setGeometry(QtCore.QRect(20, 290, 251, 131))
-        self.DCR55_180T_3.setObjectName("DCR55_180T_3")
-        self.label_11 = QtWidgets.QLabel(self.DCR55_180T_3)
+        self.SOLENOID_180T_3 = QtWidgets.QGroupBox(PlasmaCtrlDlg)
+        self.SOLENOID_180T_3.setGeometry(QtCore.QRect(20, 290, 251, 131))
+        self.SOLENOID_180T_3.setObjectName("SOLENOID_180T_3")
+        self.label_11 = QtWidgets.QLabel(self.SOLENOID_180T_3)
         self.label_11.setGeometry(QtCore.QRect(20, 60, 21, 19))
         self.label_11.setObjectName("label_11")
-        self.label_12 = QtWidgets.QLabel(self.DCR55_180T_3)
+        self.label_12 = QtWidgets.QLabel(self.SOLENOID_180T_3)
         self.label_12.setGeometry(QtCore.QRect(50, 30, 64, 19))
         self.label_12.setObjectName("label_12")
-        self.label_13 = QtWidgets.QLabel(self.DCR55_180T_3)
+        self.label_13 = QtWidgets.QLabel(self.SOLENOID_180T_3)
         self.label_13.setGeometry(QtCore.QRect(150, 30, 64, 19))
         self.label_13.setObjectName("label_13")
-        self.label_14 = QtWidgets.QLabel(self.DCR55_180T_3)
+        self.label_14 = QtWidgets.QLabel(self.SOLENOID_180T_3)
         self.label_14.setGeometry(QtCore.QRect(23, 90, 16, 20))
         self.label_14.setObjectName("label_14")
-        self.DISCHARGEVSet = QtWidgets.QLineEdit(self.DCR55_180T_3)
+        self.DISCHARGEVSet = QtWidgets.QLineEdit(self.SOLENOID_180T_3)
         self.DISCHARGEVSet.setGeometry(QtCore.QRect(40, 50, 81, 31))
         self.DISCHARGEVSet.setObjectName("DISCHARGEVSet")
-        self.DISCHARGEISet = QtWidgets.QLineEdit(self.DCR55_180T_3)
+        self.DISCHARGEISet = QtWidgets.QLineEdit(self.SOLENOID_180T_3)
         self.DISCHARGEISet.setGeometry(QtCore.QRect(40, 80, 81, 31))
         self.DISCHARGEISet.setObjectName("DISCHARGEISet")
-        self.DISCHARGEVRead = QtWidgets.QLineEdit(self.DCR55_180T_3)
+        self.DISCHARGEVRead = QtWidgets.QLineEdit(self.SOLENOID_180T_3)
         self.DISCHARGEVRead.setGeometry(QtCore.QRect(130, 50, 91, 31))
         self.DISCHARGEVRead.setObjectName("DISCHARGEVRead")
-        self.DISCHARGEIRead = QtWidgets.QLineEdit(self.DCR55_180T_3)
+        self.DISCHARGEIRead = QtWidgets.QLineEdit(self.SOLENOID_180T_3)
         self.DISCHARGEIRead.setGeometry(QtCore.QRect(130, 80, 91, 31))
         self.DISCHARGEIRead.setObjectName("DISCHARGEIRead")
         self.CloseButton = QtWidgets.QPushButton(PlasmaCtrlDlg)
@@ -134,8 +134,8 @@ class Ui_PlasmaCtrlDlg(object):
 
         self.retranslateUi(PlasmaCtrlDlg)
         self.InitalizeButton.clicked.connect(self.OnInitializeClicked)
-        self.DCR55VSet.returnPressed.connect(self.OnDCR55VSetChanged)
-        self.DCR55ISet.returnPressed.connect(self.OnDCR55ISetChanged)
+        self.SOLENOIDVSet.returnPressed.connect(self.OnSOLENOIDVSetChanged)
+        self.SOLENOIDISet.returnPressed.connect(self.OnSOLENOIDISetChanged)
         self.HEATERISet.returnPressed.connect(self.OnHEATERISetChanged)
         self.HEATERVSet.returnPressed.connect(self.OnHEATERVSetChanged)
         self.DISCHARGEISet.returnPressed.connect(self.OnDISCHARGEISetChanged)
@@ -152,17 +152,17 @@ class Ui_PlasmaCtrlDlg(object):
         self.label.setText(_translate("PlasmaCtrlDlg", "IG:"))
         self.label_2.setText(_translate("PlasmaCtrlDlg", "A:"))
         self.label_15.setText(_translate("PlasmaCtrlDlg", "B:"))
-        self.DCR55_180T.setTitle(_translate("PlasmaCtrlDlg", "DCR55-180T"))
+        self.SOLENOID_180T.setTitle(_translate("PlasmaCtrlDlg", "SOLENOID-180T"))
         self.label_3.setText(_translate("PlasmaCtrlDlg", "V: "))
         self.label_4.setText(_translate("PlasmaCtrlDlg", "Set Val"))
         self.label_5.setText(_translate("PlasmaCtrlDlg", "RB Val"))
         self.label_6.setText(_translate("PlasmaCtrlDlg", "I:"))
-        self.DCR55_180T_2.setTitle(_translate("PlasmaCtrlDlg", "Plasma Heater"))
+        self.SOLENOID_180T_2.setTitle(_translate("PlasmaCtrlDlg", "Plasma Heater"))
         self.label_7.setText(_translate("PlasmaCtrlDlg", "V: "))
         self.label_8.setText(_translate("PlasmaCtrlDlg", "Set Val"))
         self.label_9.setText(_translate("PlasmaCtrlDlg", "RB Val"))
         self.label_10.setText(_translate("PlasmaCtrlDlg", "I:"))
-        self.DCR55_180T_3.setTitle(_translate("PlasmaCtrlDlg", "Plasma Discharge"))
+        self.SOLENOID_180T_3.setTitle(_translate("PlasmaCtrlDlg", "Plasma Discharge"))
         self.label_11.setText(_translate("PlasmaCtrlDlg", "V: "))
         self.label_12.setText(_translate("PlasmaCtrlDlg", "Set Val"))
         self.label_13.setText(_translate("PlasmaCtrlDlg", "RB Val"))
@@ -173,35 +173,46 @@ class Ui_PlasmaCtrlDlg(object):
     def OnInitializeClicked(self):
         #open config file and initialize connections
         logging.debug("Initialize button clicked")
-        self.mytimer.start(2000)
+        self.mytimer.start(10000)
 
         #connections to the TDK power supplies
         #power supplies are connected via Ethernet in a chain with the heater one
         #the main one and the discharge as secondary
         #access to either one is done via the select_RS485_device() method
         
-        tdk_address = '10.0.0.1'
-        self.heater_RS485 = 0
-        self.discharge_RS485 = 1
+        tdk_address = 'TCPIP0::192.168.0.73::inst0::INSTR'
+        self.heater_RS485 = 1
+        self.discharge_RS485 = 0
         
-        self.TDK_PS = VisaHandler.VisaHandler(tdk_address)
+        #self.TDK_PS = VisaHandler.VisaHandler(tdk_address)
+
+        #set devices to output state
+        #self.TDK_PS.select_RS485_device(self.discharge_RS485)
+        #self.TDK_PS.write('OUTP:STAT 1')
+
+        #self.TDK_PS.select_RS485_device(self.heater_RS485)
+        #self.TDK_PS.write('OUTP:STAT 1')
         
-        
+        solenoid_address = 'TCPIP0::192.168.0.8::gpib0,25::INSTR'
+        self.SOLENOID_PS = VisaHandler.VisaHandler(solenoid_address,RS485_enabled=False)
+        self.SOLENOID_PS.write('OUT 1')
         
     @pyqtSlot()
-    def OnDCR55VSetChanged(self):
-        #Send cmmands to DCR55 over GPIB to change voltage setting
-        logging.debug("enter pressed on DCR55VSet")
+    def OnSOLENOIDVSetChanged(self):
+        #Send cmmands to SOLENOID over GPIB to change voltage setting
+        logging.debug("enter pressed on SOLENOIDVSet")
+        self.SOLENOID_PS.write('VSET {:3.2f}'.format(float(self.SOLENOIDVSet.text())))
         
     @pyqtSlot()
-    def OnDCR55ISetChanged(self):
-        #Send cmmands to DCR55 over GPIB to change Current setting
-        logging.debug("enter pressed on DCR55ISet")
+    def OnSOLENOIDISetChanged(self):
+        #Send cmmands to SOLENOID over GPIB to change Current setting
+        logging.debug("enter pressed on SOLENOIDISet")
+        self.SOLENOID_PS.write('ISET {:3.2f}'.format(float(self.SOLENOIDISet.text())))
         
     @pyqtSlot()
     def OnHEATERVSetChanged(self):
         #Send cmmands to Heater TDK over ethernet to change voltage setting
-        logging.debug("enter pressed on TDK#1VSet")
+        logging.info("enter pressed on TDK#1VSet")
         self.TDK_PS.select_RS485_device(self.heater_RS485)
         self.TDK_PS.write(':VOLT {:3.2f}'.format(float(self.HEATERVSet.text())))
         
@@ -209,7 +220,7 @@ class Ui_PlasmaCtrlDlg(object):
     @pyqtSlot()
     def OnHEATERISetChanged(self):
         #Send cmmands to Heater over ethernet to change current setting
-        logging.debug("enter pressed on TDK#1ISet")
+        logging.info("enter pressed on TDK#1ISet")
         self.TDK_PS.select_RS485_device(self.heater_RS485)
         self.TDK_PS.write(':CURR {:3.2f}'.format(float(self.HEATERISet.text())))
         
@@ -217,18 +228,22 @@ class Ui_PlasmaCtrlDlg(object):
     @pyqtSlot()
     def OnDISCHARGEVSetChanged(self):
         #Send cmmands to Discharge over ethernet to change voltage setting
-        logging.debug("enter pressed on TDK#2VSet")
+        logging.info("enter pressed on TDK#2VSet")
         self.TDK_PS.select_RS485_device(self.discharge_RS485)
         self.TDK_PS.write(':VOLT {:3.2f}'.format(float(self.DISCHARGEVSet.text())))
+        self.DISCHARGEVRead.setText(self.TDK_PS.query('MEAS:VOLT?'))
+        self.DISCHARGEIRead.setText(self.TDK_PS.query('MEAS:CURR?'))
         
         
     @pyqtSlot()
     def OnDISCHARGEISetChanged(self):
         #Send cmmands to TDK#2 over ethernet to change current setting
-        logging.debug("enter pressed on TDK#2ISet")
+        logging.info("enter pressed on TDK#2ISet")
         self.TDK_PS.select_RS485_device(self.discharge_RS485)
-        self.TDK_PS.write(':CURR {:3.2f}'.format(float(self.HEATERISet.text())))
-        
+        self.TDK_PS.write(':CURR {:3.2f}'.format(float(self.DISCHARGEISet.text())))
+        self.DISCHARGEVRead.setText(self.TDK_PS.query('MEAS:VOLT?'))
+        self.DISCHARGEIRead.setText(self.TDK_PS.query('MEAS:CURR?'))
+
         
     @pyqtSlot()
     def OnCloseButtonClicked(self):
@@ -238,19 +253,20 @@ class Ui_PlasmaCtrlDlg(object):
         
     @pyqtSlot()    
     def Update(self):
-        logging.debug("updating timer")
+        logging.info("updating timer")
 
         #update the values of TDIK power supplies
-        self.TDK_PS.select_RS485_device(self.discharge_RS485)
-        self.DISCHARGEVRead.setText(self.TDK_PS.query('MEAS:VOLT?'))
-        self.DISCHARGEIRead.setText(self.TDK_PS.query('MEAS:CURR?'))
+        #self.TDK_PS.select_RS485_device(self.discharge_RS485)
+        #self.DISCHARGEVRead.setText(self.TDK_PS.query('MEAS:VOLT?'))
+        #self.DISCHARGEIRead.setText(self.TDK_PS.query('MEAS:CURR?'))
 
-        self.TDK_PS.select_RS485_device(self.heater_RS485)
-        self.HEATERVRead.setText(self.TDK_PS.query('MEAS:VOLT?'))
-        self.HEATERIRead.setText(self.TDK_PS.query('MEAS:CURR?'))
-
+        #self.TDK_PS.select_RS485_device(self.heater_RS485)
+        #self.HEATERVRead.setText(self.TDK_PS.query('MEAS:VOLT?'))
+        #self.HEATERIRead.setText(self.TDK_PS.query('MEAS:CURR?'))
         
-
+        self.SOLENOIDIRead.setText(self.SOLENOID_PS.query('IOUT?').split('   ')[1])
+        self.SOLENOIDVRead.setText(self.SOLENOID_PS.query('VOUT?').split('   ')[1])
+        
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     
--- VisaHandler.py
+++ VisaHandler.py
@@ -1,6 +1,6 @@
 import logging
 import visa
-
+import time
 
 class VisaHandler(object):
 
@@ -33,16 +33,23 @@ class VisaHandler(object):
 
     def write(self, command):
         if self._connection is not None:
-            assert self._RS485_address is not None
+            if self._RS485_enabled:
+                assert self._RS485_address is not None
+                
             self._logger.debug('sending command \'{}\' to visa device (write)'.format(command))
             self._connection.write(command)
+            time.sleep(0.115)
             self.checkErrors()
-
+            
+            
     def query(self, command):
         if self._connection is not None:
-            assert self._RS485_address is not None
-            self._logger.debug('sending command \'{}\' to visa device'.format(command))
+            if self._RS485_enabled:
+                assert self._RS485_address is not None
+
+            self._logger.debug('sending command \'{}\' to visa device (read)'.format(command))
             result = self._connection.query(command)
+            time.sleep(0.115)
             self._logger.debug('recieved response \'{}\' from visa device'.format(result))
             self.checkErrors()
             return result
@@ -52,10 +59,30 @@ class VisaHandler(object):
         assert self._RS485_enabled
         if self._connection:
             if self._RS485_address != RS485_address:
-                self.write('INST:SEL {}'.format(RS485_address))
                 self._RS485_address = RS485_address
-
+                self.write('INST:SEL {:02}'.format(RS485_address))
+                self._RS485_address = RS485_address
+                self.checkErrors()
+                self._logger.info('Selected RS485 address {:02}'.format(RS485_address))
+                time.sleep(0.3)
+                
     def checkErrors(self):
         if self._connection:
-            assert self._RS485_address
-            assert self._connection.query('SYST:ERR?') is None
+            if self._RS485_enabled:
+                assert self._RS485_address is not None
+
+            try:
+                msg = self._connection.query('SYST:ERR?')
+                time.sleep(0.050)
+                assert msg.split(',')[0] is '0'
+            except AssertionError:
+                logging.warning(msg)
+
+            try:
+                msg = self._connection.query('ERR?')
+                time.sleep(0.050)
+                assert msg.split('   ')[1] is '0' or '8'
+            except AssertionError:
+                logging.warning(msg)
+
+            
